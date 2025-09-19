b8c31acf17d44afada6f3b56670fe2992e096f3d
wesley-cheung-ics4ui-01-25 <166552362+wesley-cheung-ics4ui-01-25@users.noreply.github.com>
2025-09-19 17:30:39 +0000

    update practiceproblem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,61 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
-
+	public static String pyramid(int rows) {
+		String response = "";
+		int rowlength = rows-1;
+		int count = 1;
+		while (rowlength>=0) {
+			for (int i = 0;i<rowlength;i++) {
+				response = response + " ";
+			}
+			for (int i = 0;i<((count*2)-1);i++) {
+				if (i % 2 == 0) {
+					response = response + "*";
+				} else {
+					response = response + " ";
+				}
+			}
+			rowlength-=1;
+			count += 1;
+			if (rowlength >= 0) {
+				response = response + "\n";
+			}
+		}
+		return response;
+	}
+	public static String square(int rows) {
+		String response = "";
+		if (rows <= 0) {
+			return "";
+		} else if (rows == 1) {
+			return "*";
+		} else {
+		for (int i = 0;i<rows;i++) {
+			response = response+"*";
+		}
+		response = response + "\n";
+		for (int i = 0;i<rows-2;i++) {
+			response = response+"*";
+			for (int j = 0;j<rows-2;j++) {
+				response = response+" ";
+			}
+			response = response+"*";
+			response = response + "\n";
+		}
+		for (int i = 0;i<rows;i++) {
+			response = response+"*";
+		}
+		return response;
+		}
+	}
+	public static boolean hasLowercase(String input) {
+		for (int i = 0;i<input.length();i++) {
+			if (Character.isLowerCase(input.charAt(i))) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
